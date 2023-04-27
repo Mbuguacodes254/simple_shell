54476732f304702bc8f0295955c76ba5671d5ced
Mbuguacodes254 <lizziecodes254@gmail.com>
2023-04-26 19:53:33 -0700

    n

---
+++ shell_file_loop.c
@@ -0,0 +1,160 @@
+#include "KIMBA.H"
+
+/**
+ * hsh - the chief shell loop
+ * @info: parameter & return info struct
+ * @av: argument vector from the chief()
+ *
+ * Return: 0 upon success, 1 upoon error, or code denoting error
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int builtin_ret = 0;
+
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			puts_("$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = _findbuiltin(info);
+			if (builtin_ret == -1)
+				_findcommand(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		_freeinfo(info, 0);
+	}
+	write_hist(info);
+	_freeinfo(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+ * _findbuiltin - identify a builtin command
+ * @info: return infor struct and parameter
+ *
+ * Return: -1 if builtin not found,
+ * 0 if builtin executes successfully,
+ * 1 if builtin present though not successful,
+ * 2 if builtin signals exit()
+ */
+int _findbuiltin(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", my_exit},
+		{"env", my_env},
+		{"help", my_help},
+		{"history", my_hist},
+		{"setenv", myset_env},
+		{"unsetenv", myunset_env},
+		{"cd", my_cd},
+		{"alias", my_alias},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (str_cmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+ * _findcommand - finds command in PATH
+ * @info: parameter & return info struct
+ *
+ * Return: void
+ */
+void _findcommand(info_t *info)
+{
+	char *path = NULL;
+	int i, k;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (i = 0, k = 0; info->arg[i]; i++)
+		if (!is_delim(info->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		_forkdcommand(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			_forkdcommand(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+/**
+ * _forkdcommand - forks exec thread to run command
+ * @info: the parameter & return info structure
+ *
+ * Return: void
+ */
+void _forkdcommand(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			_freeinfo(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
