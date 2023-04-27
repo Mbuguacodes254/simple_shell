c19b7e5b5b6c20b295371216d788b497e8568432
Mbuguacodes254 <lizziecodes254@gmail.com>
2023-04-26 19:42:51 -0700

    a

---
+++ get_inf_o.c
@@ -0,0 +1,73 @@
+#include "KIMBA.H"
+
+/**
+ * __clearinfo - this initializes the info_t struct
+ * @info: the struct address
+ */
+void __clearinfo(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * _setinfo - the initializes info_t struct
+ * @info: the struct address
+ * @av: the argument vector
+ */
+void _setinfo(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = str_duplicate(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		rep_variiables(info);
+	}
+}
+
+/**
+ * _freeinfo - frees the info_t struct fields
+ * @info: the struct address
+ * @all: is true if freeing all fields
+ */
+void _freeinfo(info_t *info, int all)
+{
+	f_free(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			_free_list(&(info->env));
+		if (info->history)
+			_free_list(&(info->history));
+		if (info->alias)
+			_free_list(&(info->alias));
+		f_free(info->environ);
+			info->environ = NULL;
+		b_free((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
